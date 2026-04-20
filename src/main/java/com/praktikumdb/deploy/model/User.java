c40aa661e2a6e5968dd31e60e0146f9b3b771257
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-04-20 09:59:16 +0700

    Menambahkan class id,name dan nim

--- src/main/java/com/praktikumdb/deploy/model/User.java
+++ src/main/java/com/praktikumdb/deploy/model/User.java
@@ -1,6 +1,8 @@
 package com.praktikumdb.deploy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,4 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
 }
