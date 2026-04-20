16b62769e0e0ffb223f7579f37eaac199bb97931
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-04-20 09:57:28 +0700

    Menambahkan import didalam file user

---
+++ src/main/java/com/praktikumdb/deploy/model/User.java
@@ -0,0 +1,17 @@
+package com.praktikumdb.deploy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20230140071_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+}
